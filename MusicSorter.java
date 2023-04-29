4bdb95e67df4ea5345cee3207d1fb66931d2a245
comfortabletrain <112996430+comfortabletrain@users.noreply.github.com>
2023-04-28 23:01:16 -0400

    just a starter for what will be written

---
+++ MusicSorter.java
@@ -0,0 +1,21 @@
+/*
+Name: Daren Van Dusen
+Description: the main file for the classical music database
+*/
+
+
+public class MusicSorter{
+	public static void main(String[] args){
+	
+	printMenu();
+
+	}
+
+	public static void printMenu(){
+	
+	System.out.println("This code has not been written yet :)");
+
+	}
+
+
+}
